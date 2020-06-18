ac054a077927d6338bd371b2860baa38e5650b8a
mich-ch <mixalxatzi@gmail.com>
2020-06-18 17:35:42 +0300

    Create virtual items

--- ApplyApp/Program.cs
+++ ApplyApp/Program.cs
@@ -6,7 +6,36 @@ namespace ApplyApp
     {
         static void Main(string[] args)
         {
-           
+            /*   Candidate
+                 Create: Candidate
+                         Skills
+                         Education
+                         Experience
+                         Request
+
+                 Show:   Candidate
+                         Skills
+                         Education
+                         Experience
+                         Request
+            */
+
+            /*   HRManager
+                 Create: HRManager
+                         Skills
+                         Education
+                         Experience
+                         Request
+                         JobOffer
+                         Accept/Reject
+
+                 Show:   Candidate
+                         Skills
+                         Education
+                         Experience
+                         Request
+                         JobOffers
+            */
         }
     }
 }
